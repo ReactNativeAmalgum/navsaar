e6d658dbcd16dac9d7429e7cf1810f0a251821f5
Kamleshyadav2319 <118962903+Kamleshyadav2319@users.noreply.github.com>
2025-09-02 20:46:22 +0530

    feat: small changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 "use client";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
-import { useEffect } from 'react';
-import ZoomImage from './components/ZoomImage';
+import { useEffect } from "react";
+import ZoomImage from "./components/ZoomImage";
 
 export default function Home() {
   useEffect(() => {
@@ -13,15 +13,15 @@ export default function Home() {
   return (
     <>
       <Navbar />
-            <ZoomImage />
+      <ZoomImage />
 
       {/* <Home /> */}
-      {/* <div className="container mt-5" style={{height:'150%'}} >
+      <div className="container mt-5" style={{ height: "150%" }}>
         <h1>Welcome to Navsaar</h1>
         <p>This is the homepage.</p>
 
         <div style={{ height: "150vh" }}></div>
-      </div> */}
+      </div>
     </>
   );
 }
